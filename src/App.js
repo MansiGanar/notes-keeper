3df63653d642cdc29cb5c7bcfb229053fd01c8bc
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2022-11-07 02:54:36 +0100

    completed notes keeper

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+import CreateArea from "./components/CreateArea";
+import { v4 as uuidv4 } from "uuid";
+function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(Newnote) {
+    console.log(Newnote);
+    setNotes((previousNotes) => {
+      return [...previousNotes, Newnote];
+    });
+  }
+  function deleteNote(id) {
+    setNotes((previousNotes) => {
+      return previousNotes.filter((noteItem, index) => {
+        return noteItem.id !== id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note) => (
+        <Note
+          key={uuidv4()}
+          title={note.title}
+          content={note.content}
+          id={note.id}
+          onDelete={deleteNote}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    key: 1,
+    title: "mansi",
+    content: "do something",
+  },
+  {
+    key: 2,
+    title: "mansi",
+    content: "do something",
+  },
+  {
+    key: 3,
+    title: "mansi",
+    content: "do something",
+  },
+  {
+    key: 4,
+    title: "mansi",
+    content: "do something",
+  },
+  {
+    key: 5,
+    title: "mansi",
+    content: "do something",
+  },
+];
+
+export default notes;
